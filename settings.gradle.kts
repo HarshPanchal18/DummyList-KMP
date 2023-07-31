7cbcd7257459cfaaaa5f8626204a15aa6c58d1b6
Harsh Panchal <harshhhh1803@gmail.com>
2023-07-31 12:53:32 +0530

    first commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "DummyList-KMP"
+include(":androidApp")
+include(":shared")
